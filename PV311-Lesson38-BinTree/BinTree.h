a1db1fd7a4288f4c13587d9e15eca68b5be5e005
lapshuntanya <lapshun.tatiana@gmail.com>
2024-07-05 20:03:50 +0300

    add class Tree

---
+++ PV311-Lesson38-BinTree/BinTree.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <iostream>
+using std::cout;
+using std::endl;
+
+namespace BinTree {
+
+	template <typename T>
+	class Node {
+	public:
+		T info;
+		Node* left;
+		Node* right;
+
+		Node(T value) { info = value; left = right = nullptr; }
+		void showNode() {
+			cout << "INFO: " << info << endl;
+			cout << "\t Left: " << left << " Right: " << right << endl;
+		}
+	};
+
+	template <typename U>
+	class Tree {
+		Node<U>* root;
+	public:
+		Tree() {
+			root = nullptr;
+		}
+		~Tree() {
+
+		}
+
+		void insert(U value) {
+			Node<U>* el = new Node<U>(value);
+
+			if (root == nullptr) {
+				root = el;
+			}
+			else {
+				Node<U>* p = root;
+				Node<U>* place = root;
+				//Шукаємо місце для створеного вузла
+				while (p != nullptr)
+				{
+					/*if (el->info < p->info)
+						p = p->left;
+					else
+						p = p->right;*/
+
+					place = p;
+					p = (el->info < p->info) ? p->left : p->right;
+				}
+
+				//Привязка нового вузла
+				if (el->info < place->info)
+					place->left = el;
+				else
+					place->right = el;
+			}
+		}
+	};
+
+}
